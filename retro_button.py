6c593226759346d2ce268dbcd5df2f726f6713e7
bendikMichal <mbendik97@gmail.com>
2024-09-12 11:06:39 +0200

    window is maximizable/minimizable and movable

--- retro_button.py
+++ retro_button.py
@@ -4,7 +4,7 @@ import pygame
 
 class RetroButton:
     ICON_PATH = os.path.abspath(".") + "/ui_icons"
-    def __init__ (self, name: str, x: int, y: int, w: int = 32, h: int = 32, colors: list[tuple] = [(0, 0, 0)] * 2, onclick = None, anchors: list[int] = [0, 0, 0, 0]):
+    def __init__ (self, name: str, x: int, y: int, w: int = 32, h: int = 32, colors: list[tuple] = [(0, 0, 0)] * 2, onclick = None, onpressed = None, anchors: list[int] = [0, 0, 0, 0]):
         self.name = name
         self.x = x
         self.y = y
@@ -14,19 +14,25 @@ class RetroButton:
         self.colors = colors
         
         self.onclick = onclick
+        self.onpressed = onpressed
         self.anchors = anchors
 
-        self.path = self.ICON_PATH + "/" + name + ".png"
-        self.img = pygame.image.load(self.path).convert_alpha()
+        self.load_img()
         
         self.focused = False
         self.pressed = False
         self.__prev_pressed = self.pressed
+
+        self.origin_press = (0, 0)
+
+    def load_img (self):
+        self.path = self.ICON_PATH + "/" + self.name + ".png"
+        self.img = pygame.image.load(self.path).convert_alpha()
     
     def get_rect (self, win_size):
         r = [self.x, self.y, self.w, self.h]
-        if self.anchors[0]: r[0] = win_size[0] - r[0]
-        if self.anchors[1]: r[1] = win_size[1] - r[1]
+        if self.anchors[0]: r[0] = win_size[0] - r[0] - r[2]
+        if self.anchors[1]: r[1] = win_size[1] - r[1] - r[3]
 
         return pygame.Rect(r)
 
@@ -36,6 +42,13 @@ class RetroButton:
         self.focused = r.collidepoint(mouse_pos)
         self.__prev_pressed = self.pressed
         self.pressed = self.focused and mouse_btns[0]
+
+        # pressed
+        if self.pressed and self.onpressed:
+            if not self.__prev_pressed:
+                self.origin_press = mouse_pos
+
+            self.onpressed(self)
 
         # clicked
         if not self.pressed and self.__prev_pressed and self.focused:
@@ -52,3 +65,55 @@ class RetroButton:
         win.blit(self.img, [r.x, r.y])
 
 
+class MoveButton(RetroButton):
+    def __init__ (self, x: int, y: int, w: int = 32, h: int = 32, colors: list[tuple] = [(0, 0, 0)] * 2, border_color: tuple = (0, 0, 0), shadow_color: tuple = (0, 0, 0), onclick = None, onpressed = None, anchors: list[int] = [0, 0, 0, 0]):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.rect = pygame.Rect(x, y, w, h)
+        self.colors = colors
+        self.border_color = border_color
+        self.shadow_color = shadow_color
+        
+        self.onclick = onclick
+        self.onpressed = onpressed
+        self.anchors = anchors
+        
+        self.focused = False
+        self.pressed = False
+        self.__prev_pressed = self.pressed
+
+        self.origin_press = (0, 0)
+    
+    def get_rect (self, win_size):
+        return super().get_rect(win_size)
+
+    def update (self, mouse_pos: list[int], mouse_btns: list[bool], win_size):
+        r = self.get_rect(win_size)
+
+        self.focused = r.collidepoint(mouse_pos) or self.pressed
+        self.__prev_pressed = self.pressed
+        self.pressed = self.focused and mouse_btns[0]
+
+        # pressed
+        if self.pressed and self.onpressed:
+            if not self.__prev_pressed:
+                self.origin_press = mouse_pos
+
+            self.onpressed(self)
+
+        # clicked
+        if not self.pressed and self.__prev_pressed and self.focused:
+            if self.onclick:
+                self.onclick(self)
+
+    def render (self, win, win_size):
+        r = self.get_rect(win_size)
+
+        if self.pressed: r.y -= 1
+
+        pygame.draw.rect(win, self.colors[int(self.focused)], r)
+        pygame.draw.rect(win, self.shadow_color, (r.x, r.y + r.h - 4, r.w, 4))
+        pygame.draw.rect(win, self.border_color, r, 1)
+        pygame.draw.line(win, self.border_color, (r.x, r.y + r.h), (r.x + r.w - 1, r.y + r.h), 1)
--- retro_gui.py
+++ retro_gui.py
@@ -1,9 +1,27 @@
 
 import os
 import pygame
-from retro_button import RetroButton
+from pygame.version import vernum
+from retro_button import MoveButton, RetroButton
 
 pygame.init()
+
+def init ():
+    print(vernum.major, vernum.minor)
+    pass
+    if vernum.major < 2:
+        print("Required major version of pygame-ce is >= 2")
+        os._exit(1)
+    if vernum.major == 2 and vernum.minor < 5:
+        print("Required minor version of pygame-ce is >= 5")
+        os._exit(1)
+
+init()
+
+def move_check ():
+    if not pygame.display.get_window_position or not pygame.display.set_window_position:
+        print("Unable to get/set window position, update pygame-ce version! Minimum pygame-ce version is >= 2.5.x")
+        os._exit(1)
 
 class Colors:
     BG = (179, 156, 174)
@@ -11,10 +29,14 @@ class Colors:
     TEXT = (15, 11, 12)
     CLOSE = (232, 127, 141)
     CLOSE_HOVER = (235, 153, 165)
+    SHADOW = (158, 133, 152)
 
 UI_FPS = 60
 WIN_BORDER_SIZE = 2
+BAR_SIZE = 48
 WINDOW_FLAGS = pygame.NOFRAME | pygame.RESIZABLE
+# WINDOW_FLAGS = pygame.RESIZABLE
+
 
 __internal_clock = pygame.Clock()
 ui_tick = lambda: __internal_clock.tick(UI_FPS)
@@ -31,19 +53,48 @@ def __close_app (_):
     app_state.running = False
 
 def __maximize_app (btn):
-    os.environ['SDL_VIDEO_CENTERED'] = "1"
-    pygame.display.set_mode((__info.current_w, __info.current_h), WINDOW_FLAGS)
+    # os.environ['SDL_VIDEO_CENTERED'] = "1"
+    pygame.display.set_mode((__info.current_w, __info.current_h - BAR_SIZE), WINDOW_FLAGS)
+    pygame.display.set_window_position((0, 0))
+    btn.name = "windowize"
+    btn.load_img()
+    btn.onclick = __windowize_app
 
-def get_window (w: int, h: int, caption: str):
+def __minimize_app (btn):
+    pygame.display.iconify()
+
+def __move_window (btn):
+    move_check()
+    x, y = pygame.display.get_window_position()
+    mpos = pygame.mouse.get_pos()
+    dist_x = mpos[0] - btn.origin_press[0]
+    dist_y = mpos[1] - btn.origin_press[1]
+    x += dist_x
+    y += dist_y
+
+    pygame.display.set_window_position((x, y))
+
+def __windowize_app (btn):
+    os.environ['SDL_VIDEO_CENTERED'] = "1"
+    pygame.display.set_mode(__win_size, WINDOW_FLAGS)
+    btn.name = "maximize"
+    btn.load_img()
+    btn.onclick = __maximize_app
+
+
+def get_window (w: int, h: int, caption: str, icon: str | None = None):
     global __win_size
     __win_size = (w, h)
     win = pygame.display.set_mode((w, h), WINDOW_FLAGS)
     pygame.display.set_caption(caption)
+    if icon: pygame.display.set_icon(pygame.image.load(icon).convert_alpha())
 
     icon_size = 24
     pad = 6
-    create_button("close", icon_size + pad, pad, w = icon_size, h = icon_size, colors = [Colors.CLOSE, Colors.CLOSE_HOVER], anchors = [1, 0], onclick = __close_app)
-    create_button("maximize", (icon_size + pad) * 2, pad, w = icon_size, h = icon_size, anchors = [1, 0], onclick = __maximize_app)
+    create_button("close", pad, pad, w = icon_size, h = icon_size, colors = [Colors.CLOSE, Colors.CLOSE_HOVER], anchors = [1, 0], onclick = __close_app)
+    create_button("maximize", icon_size + pad * 2, pad, w = icon_size, h = icon_size, anchors = [1, 0], onclick = __maximize_app)
+    create_button("minimize", icon_size * 2 + pad * 3, pad, w = icon_size, h = icon_size, anchors = [1, 0], onclick = __minimize_app)
+    create_move_button(icon_size * 3 + pad * 4, pad, w = w - (52 + (icon_size + pad) * 4), h = 20, anchors = [1, 0], onpressed = __move_window)
     return win
 
 def window_update (window: pygame.Surface):
@@ -71,11 +122,20 @@ def window_render (window: pygame.Surface):
 
 # UI stuff ======================================
 
-def create_button (name: str, x: int, y: int, w: int = 32, h: int = 32, colors: list[tuple] = [Colors.BG, Colors.LIGHT_BG], onclick = None, anchors: list[int] = [0, 0, 0, 0]):
+def create_button (name: str, x: int, y: int, w: int = 32, h: int = 32, colors: list[tuple] = [Colors.BG, Colors.LIGHT_BG], onclick = None, onpressed = None, anchors: list[int] = [0, 0, 0, 0]):
     """
     colors: list[tuple] : [normal_color, hover_color] # eg. [(0, 0, 0), (255, 255, 255)] 
     anchors: list[int]  : [right, bottom] # eg. [1, 1]; when used with x = 4, y = 4 the button will be 4 pixels from right edge and 4 pixels from bottom 
     """
-    _btn = RetroButton(name, x, y, w, h, colors, onclick, anchors)
+    _btn = RetroButton(name, x, y, w, h, colors, onclick, onpressed, anchors)
+    app_state.widgets.append(_btn)
+    return _btn
+
+def create_move_button (x: int, y: int, w: int = 32, h: int = 32, colors: list[tuple] = [Colors.BG, Colors.LIGHT_BG], border_color: tuple = Colors.TEXT, shadow_color: tuple = Colors.SHADOW, onclick = None, onpressed = None, anchors: list[int] = [0, 0, 0, 0]):
+    """
+    colors: list[tuple] : [normal_color, hover_color] # eg. [(0, 0, 0), (255, 255, 255)] 
+    anchors: list[int]  : [right, bottom] # eg. [1, 1]; when used with x = 4, y = 4 the button will be 4 pixels from right edge and 4 pixels from bottom 
+    """
+    _btn = MoveButton(x, y, w, h, colors, border_color, shadow_color, onclick, onpressed, anchors)
     app_state.widgets.append(_btn)
     return _btn
